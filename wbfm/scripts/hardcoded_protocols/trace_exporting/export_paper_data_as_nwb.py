92bc42334e209093437efa8a70b56f5be7e7caf6
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2026-03-27 13:37:07 +0100

    Add options; #1102

--- wbfm/scripts/hardcoded_protocols/trace_exporting/export_paper_data_as_nwb.py
+++ wbfm/scripts/hardcoded_protocols/trace_exporting/export_paper_data_as_nwb.py
@@ -10,14 +10,19 @@ from wbfm.utils.nwb.utils_nwb_export import nwb_using_project_data
 if __name__ == '__main__':
     # Get args
     parser = argparse.ArgumentParser(description='Export traces in nwb format')
-    # Debug mode
+    parser.add_argument('--include_image_data', action='store_true', help='Whether to include image data in the export')
     parser.add_argument('--debug', action='store_true', help='Debug mode')
     args = parser.parse_args()
 
     DEBUG = args.debug
+    include_image_data = args.include_image_data
 
     # Export to hardcoded locations
-    parent_dir = '/lisc/user/fieseler/zimmer/fieseler/paper/nwb'
+    parent_dir = '/lisc/data/scratch/neurobiology/zimmer/fieseler/paper'
+    if include_image_data:
+        parent_dir = os.path.join(parent_dir, 'with_images')
+    else:
+        parent_dir = os.path.join(parent_dir, 'no_images')
     all_suffixes = ['gfp', '', 'mutant', 'immob']
 
     for suffix in tqdm(all_suffixes):
@@ -37,7 +42,7 @@ if __name__ == '__main__':
             try:
                 print("=" * 50)
                 print(f'Exporting {name} to {this_folder}')
-                nwb_using_project_data(project, include_image_data=False, output_folder=this_folder)
+                nwb_using_project_data(project, include_image_data=include_image_data, output_folder=this_folder)
             except Exception as e:
                 print(f'Error exporting {name}: {e}')
                 continue
